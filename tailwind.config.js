a02222ad1b11c2f1d68d4799d67b33cc8836d405
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-11-08 01:04:11 +0200

    Update tailwind config file format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -10,69 +8,102 @@ module.exports = {
         'soft-orange': '#F6A560',
         'soft-rose': '#F39E9E',
         'soft-red': '#EB7565',
-        'cyan': '#61C4B7',
+        cyan: '#61C4B7',
         'dark-mauve': '#552049',
-        'black': '#030303',
+        black: '#030303',
         'grayish-orange': '#7A746E',
         'off-white': '#FFF7F0',
-        'white': '#FFFFFF'
+        white: '#FFFFFF',
       },
       fontFamily: {
         'plus-jakarta': ['Plus Jakarta Sans', 'sans-serif'],
       },
       fontSize: {
-        'sm-mob': ['14px', {
-          lineHeight: '28px',
-          fontWeight: '700'
-        }],
-        'sm-dsk': ['16px', {
-          lineHeight: '28px',
-          fontWeight: '700'
-        }],
-        'base-mob': ['16px', {
-          lineHeight: '26px',
-          fontWeight: '500'
-        }],
-        'base-dsk': ['18px', {
-          lineHeight: '28px',
-          fontWeight: '500'
-        }],
-        '2xl-mob': ['24px', {
-          lineHeight: '30px',
-          fontWeight: '700'
-        }],
-        '3xl-mob': ['26px', {
-          lineHeight: '33px',
-          fontWeight: '700'
-        }],
-        '3xl-dsk': ['32px', {
-          lineHeight: '40px',
-          fontWeight: '700'
-        }],
-        '4xl-mob': ['36px', {
-          lineHeight: '45px',
-          fontWeight: '700'
-        }],
-        '4xl-dsk': ['40px', {
-          lineHeight: '51px',
-          fontWeight: '700'
-        }],
-        '5xl-dsk': ['44px', {
-          lineHeight: '56px',
-          fontWeight: '700'
-        }],
-        '6xl-dsk': ['56px', {
-          lineHeight: '70px',
-          fontWeight: '700'
-        }]
+        'sm-mob': [
+          '14px',
+          {
+            lineHeight: '28px',
+            fontWeight: '700',
+          },
+        ],
+        'sm-dsk': [
+          '16px',
+          {
+            lineHeight: '28px',
+            fontWeight: '700',
+          },
+        ],
+        'base-mob': [
+          '16px',
+          {
+            lineHeight: '26px',
+            fontWeight: '500',
+          },
+        ],
+        'base-dsk': [
+          '18px',
+          {
+            lineHeight: '28px',
+            fontWeight: '500',
+          },
+        ],
+        '2xl-mob': [
+          '24px',
+          {
+            lineHeight: '30px',
+            fontWeight: '700',
+          },
+        ],
+        '3xl-mob': [
+          '26px',
+          {
+            lineHeight: '33px',
+            fontWeight: '700',
+          },
+        ],
+        '3xl-dsk': [
+          '32px',
+          {
+            lineHeight: '40px',
+            fontWeight: '700',
+          },
+        ],
+        '4xl-mob': [
+          '36px',
+          {
+            lineHeight: '45px',
+            fontWeight: '700',
+          },
+        ],
+        '4xl-dsk': [
+          '40px',
+          {
+            lineHeight: '51px',
+            fontWeight: '700',
+          },
+        ],
+        '5xl-dsk': [
+          '44px',
+          {
+            lineHeight: '56px',
+            fontWeight: '700',
+          },
+        ],
+        '6xl-dsk': [
+          '56px',
+          {
+            lineHeight: '70px',
+            fontWeight: '700',
+          },
+        ],
       },
       height: {
-        '90': '360px'
+        90: '360px',
       },
       padding: {
-        '19': '76px'
-      }
+        19: '76px',
+      },
     },
   },
   plugins: [],
-}
+};
