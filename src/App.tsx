492f53c17e5d256bd900a2daed018af5a2e287c4
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-11-08 13:54:54 +0200

    Update App Component
    Added desktop styles

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="App bg-off-white py-4 md:py-9 font-plus-jakarta">
-      <header className="App-header mx-4 md:mx-10 flex flex-row flex-nowrap justify-between items-center">
+      <header className="App-header mx-4 md:mx-10 xl:mx-40 flex flex-row flex-nowrap justify-between items-center">
         <img src={logo} alt="Company Logo" className="w-12 md:w-16" />
         <a
           href="https://www.google.com"
@@ -30,14 +30,14 @@ function App() {
         </a>
       </header>
       <main className="pt-8 md:pt-16">
-        <h1 className="mx-4 md:mx-10 text-black text-4xl-mob md:text-5xl-dsk text-center">
+        <h1 className="mx-4 md:mx-10 xl:mx-40 text-black text-4xl-mob md:text-5xl-dsk xl:text-6xl-dsk text-center">
           Design solutions made easy
         </h1>
-        <p className="text-base-mob md:text-base-dsk text-center mx-11 md:mx-24 mt-4 md:mt-7 mb-8 md:mb-16 text-grayish-orange">
+        <p className="text-base-mob md:text-base-dsk text-center mx-11 md:mx-24 xl:mx-89 mt-4 md:mt-7 mb-8 md:mb-16 xl:mb-20 text-grayish-orange">
           With over ten years of experience in various design disciplines, I'm
           your one-stop shop for your design needs.
         </p>
-        <div className="grid grid-rows-6 md:grid-rows-3 grid-cols-2 md:grid-cols-4 gap-6 text-2xl-mob text-white mx-4 md:mx-10 mb-24 md:mb-28">
+        <div className="grid grid-rows-6 md:grid-rows-3 xl:grid-rows-2 grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-6 text-2xl-mob text-white mx-4 md:mx-10 xl:mx-40 mb-24 md:mb-28 xl:mb-34">
           <div className="flex flex-col justify-between bg-soft-purple p-6 rounded-lg row-start-1 row-end-3 col-span-full md:col-span-2">
             <div className="flex flex-row justify-end">
               <img src={graphicImg} alt="" className="w-32 mb-24" />
@@ -52,45 +52,45 @@ function App() {
             <p>UI/UX</p>
           </div>
 
-          <div className="flex flex-col justify-between bg-soft-rose p-6 rounded-lg row-start-3 row-end-4 col-start-2 md:row-start-1 md:row-end-2 md:col-start-4">
+          <div className="flex flex-col justify-between bg-soft-rose p-6 rounded-lg row-start-3 row-end-4 col-start-2 md:row-start-1 md:row-end-2 md:col-start-4 xl:col-end-4">
             <div className="flex flex-row justify-end">
               <img src={appImg} alt="" className="w-16 mb-9" />
             </div>
             <p>Apps</p>
           </div>
 
-          <div className="flex flex-col justify-between bg-soft-red p-6 rounded-lg row-start-4 row-end-5 col-span-full md:row-start-2 md:row-end-3 md:col-span-2">
+          <div className="flex flex-col justify-between bg-soft-red p-6 rounded-lg row-start-4 row-end-5 col-span-full md:row-start-2 md:row-end-3 md:col-span-2 xl:col-start-3 xl:col-end-5">
             <div className="flex flex-row justify-end">
               <img src={illustrationsImg} alt="" className="w-32 mb-4" />
             </div>
             <p>Illustrations</p>
           </div>
 
-          <div className="flex flex-col justify-between bg-cyan p-6 rounded-lg row-start-5 row-end-6 col-span-full md:row-start-3 md:row-end-3 md:col-span-2">
+          <div className="flex flex-col justify-between bg-cyan p-6 rounded-lg row-start-5 row-end-6 col-span-full md:row-start-3 md:row-end-3 md:col-span-2 xl:row-start-1 xl:row-end-2">
             <div className="flex flex-row justify-end">
               <img src={photographyImg} alt="" className="w-40 mb-9" />
             </div>
             <p>Photography</p>
           </div>
 
-          <div className="flex flex-col justify-between bg-dark-mauve p-6 rounded-lg row-start-6 col-span-full md:row-start-3 md:row-end-3 md:col-span-2">
+          <div className="flex flex-col justify-between bg-dark-mauve p-6 rounded-lg row-start-6 col-span-full md:row-start-3 md:row-end-3 md:col-span-2 xl:row-start-2 xl:row-end-3">
             <div className="flex flex-row justify-end">
               <img src={motionImg} alt="" className="w-32 mb-9" />
             </div>
             <p>Motion Graphics</p>
           </div>
         </div>
-        <div className="mx-4 flex flex-col items-center md:flex-row md:justify-between md:items-start">
+        <div className="mx-4 flex flex-col items-center md:flex-row md:justify-between md:items-start xl:items-center xl:xl:mx-40">
           <img
             src={personImg}
             alt=""
-            className="w-5/6 mb-11 md:w-3/6 md:-ml-16 md:mr-16 md:mb-0"
+            className="w-5/6 mb-11 md:w-3/6 md:-ml-16 md:mr-16 md:mb-0 xl:ml-0 xl:w-2/5 xl:mr-32"
           />
-          <div className="flex flex-col items-center md:items-start md:mr-10">
-            <p className="text-center text-3xl-mob text-black mb-6 md:text-left md:text-3xl-dsk">
+          <div className="flex flex-col items-center md:items-start md:mr-10 xl:justify-center xl:mr-0 xl:mt-auto">
+            <p className="text-center text-3xl-mob text-black mb-6 md:text-left md:text-3xl-dsk xl:text-4xl-dsk xl:mb-9">
               I'm Amy, and I'd love to work on your next project
             </p>
-            <p className="text-center text-base-mob text-grayish-orange mb-6 md:text-base-dsk md:text-left">
+            <p className="text-center text-base-mob text-grayish-orange mb-6 md:text-base-dsk md:text-left xl:mb-8">
               I love working with others to create beautiful design solutions.
               I've designed everything from brand illustrations to complete
               mobile apps. I'm also handy with a camera!
@@ -103,7 +103,7 @@ function App() {
             </a>
           </div>
         </div>
-        <p className="text-center text-black text-2xl-mob mb-9 md:mb-14 md:text-3xl-dsk">
+        <p className="text-center text-black text-2xl-mob mb-9 md:mb-14 md:text-3xl-dsk md:mt-7 xl:mt-24">
           My Work
         </p>
         <div
@@ -159,15 +159,17 @@ function App() {
             <img src={arrowRight} alt="" />
           </div>
         </div>
-        <div className="mx-4 flex flex-col items-center bg-black px-6 py-12 rounded-lg md:pt-14 md:pb-16 md:mx-10 md:px-24">
-          <p className="text-off-white text-3xl-mob text-center md:text-3xl-dsk">
-            Book a call with me
-          </p>
-          <p className="text-off-white text-base-mob text-center my-9 md:text-base-dsk">
-            I'd love to have a chat to see how I can help you. The best first
-            step is for us to discuss your project during a free consultation.
-            Then we can move forward from there.
-          </p>
+        <div className="mx-4 flex flex-col items-center bg-black px-6 py-12 rounded-lg md:pt-14 md:pb-16 md:mx-10 xl:mx-40 md:px-24 xl:flex-row xl:justify-between">
+          <div className='flex flex-col items-center xl:items-start xl:w-7/12'>
+            <p className="text-off-white text-3xl-mob text-center md:text-3xl-dsk xl:text-left xl:text-4xl-dsk">
+              Book a call with me
+            </p>
+            <p className="text-off-white text-base-mob text-center my-9 md:text-base-dsk xl:text-left xl:text-base-dsk">
+              I'd love to have a chat to see how I can help you. The best first
+              step is for us to discuss your project during a free consultation.
+              Then we can move forward from there.
+            </p>
+          </div>
           <a
             href="https://www.google.com"
             className="text-off-white text-sm-mob bg-soft-red hover:bg-soft-orange py-2 px-7 rounded-full md:py-4 md:px-12 md:text-sm-dsk"
@@ -176,7 +178,7 @@ function App() {
           </a>
         </div>
       </main>
-      <footer className="mx-4 mb-4 pt-10 flex flex-row flex-nowrap justify-between items-center md:pt-14">
+      <footer className="mx-4 mb-4 pt-10 flex flex-row flex-nowrap justify-between items-center md:pt-14 md:mx-10 xl:mx-40">
         <img src={logo} alt="" className="w-12 md:w-16" />
         <a
           href="https://www.google.com"
